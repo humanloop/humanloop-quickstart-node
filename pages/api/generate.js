eefff87f4ea606f6f12d77891d0580cc6bf21195
Jordan Burgess <jordan@humanloop.com>
2022-08-30 16:33:41 +0100

    Clean up openai code

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,17 +1,4 @@
-import { Configuration, OpenAIApi } from "openai";
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
 export default async function (req, res) {
-  // const completion = await openai.createCompletion({
-  //   model: "text-davinci-002",
-  //   prompt: generatePrompt(req.body.animal),
-  //   temperature: 0.6,
-  // });
-  console.log(process.env.HUMANLOOP_API_KEY);
   const completion = await fetch(
     "https://neostaging.humanloop.ml/models/generate",
     {
@@ -43,19 +30,5 @@ export default async function (req, res) {
   );
 
   const data = await completion.json();
-  console.log(JSON.stringify(data, null, 2));
   res.status(200).json({ result: data.logs[0].output });
-}
-
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
 }
