15be691592907a5d6286f296c51865e7455b150c
Peter Hayes <ucabpnh@ucl.ac.uk>
2022-10-07 00:03:14 +0100

    Update with latest generate

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 
 export default async function (req, res) {
   const completion = await fetch(
-    "https://api.humanloop.com/models/generate",
+    "https://api.humanloop.com/v1/generate",
     {
       method: "POST",
       headers: {
@@ -9,20 +9,6 @@ export default async function (req, res) {
         "X-API-KEY": process.env.HUMANLOOP_API_KEY,
       },
       body: JSON.stringify({
-        model: "text-davinci-002",
-        prompt_template: `Answer the question like Paul Graham from YCombinator.
-Question: How do I know if I am ready for launch?
-Answer: You should have launched already###
-Question: How quickly should I scale my team? 
-Answer: Spend little and get Ramen profitable before scaling the team###
-Question: {{question}}
-Answer:`,
-        parameters: {
-          temperature: 0.6,
-          n: 1,
-          max_tokens: 100,
-          stop: "###"
-        },
         inputs: { question: req.body.question },
         source: "test-app",
         project: "ask-paul-graham",
